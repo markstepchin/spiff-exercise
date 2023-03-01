b6baf48b69131cdfeced1c4ace537c4097765e6c
Mark Stepchin <markstepchin@gmail.com>
2023-03-01 11:48:56 -0800

    add progress bar

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,22 +1,15 @@
 import React from "react";
 import Exercise from "../exercise/Exercise";
+import ProgressBar from "../components/ProgressBar";
 
-const ProgressBarExercise = () => {
-  return (
-    <div className="progress-bar-exercise">
-      <Exercise
-        solution={<Solution />}
-        specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/1"
-        title="Progress Bar Exercise"
-      />
-    </div>
-  );
-};
+const ProgressBarExercise = () => (
+  <div className="progress-bar-exercise">
+    <Exercise
+      solution={<ProgressBar progress={50} />}
+      specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/1"
+      title="Progress Bar Exercise"
+    />
+  </div>
+);
 
 export default ProgressBarExercise;
-
-// ----------------------------------------------------------------------------------
-
-const Solution = () => {
-  return <div>Add solution here</div>;
-};

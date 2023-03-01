cb9e1c1843fe46288abd8801041ec4d6aa10e693
Mark Stepchin <markstepchin@gmail.com>
2023-03-01 11:52:49 -0800

    simulate progress bar loading

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Exercise from "../exercise/Exercise";
-import ProgressBar from "../components/ProgressBar";
+import SimulateProgressBar from "../components/SimulateProgress";
 
 const ProgressBarExercise = () => (
   <div className="progress-bar-exercise">
     <Exercise
-      solution={<ProgressBar progress={50} />}
+      solution={<SimulateProgressBar />}
       specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/1"
       title="Progress Bar Exercise"
     />
